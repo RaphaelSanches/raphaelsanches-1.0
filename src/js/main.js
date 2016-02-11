080af2ae07b43d534eeee4e94e8fababf804cbea
Raphael Sanches <raphael.lsanches@hotmail.com>
2016-02-11 00:27:18 -0200

    Adição de alguns scripts do portfólio e do header

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@
 
 	var bannerHeight = $('#banner').height()
 
+	//Menu function
 	$(window).scroll(function(){
 		if($(this).scrollTop() > bannerHeight){
 			$('.nav-bar').removeClass('not-scrolled').addClass('scrolled');
@@ -12,6 +13,22 @@
 		else{
 			$('.nav-bar').removeClass('scrolled').addClass('not-scrolled');
 		}
+	});
+
+	//Button to down
+	$('.btn-down').click(function(e){
+		e.preventDefault();
+    	$('html, body').animate({
+			scrollTop: bannerHeight
+        }, 900);
+	});
+
+	//Portfolio
+	$('#portfolio').on('click', '.portfolio-item', function(e){
+		e.preventDefault();
+		$('.portfolio-item').addClass('blurred');
+		$(this).removeClass('blurred');
+
 	});
 
  }) ();
